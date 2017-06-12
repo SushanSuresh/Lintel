49501fe1eafb9b4fbf91c46c0f549f12dfce0d44
root <root@ip-172-31-11-230.ap-south-1.compute.internal>
2017-06-12 20:14:36 +0000

    Paramter password

--- drop.php
+++ drop.php
@@ -1,15 +1,20 @@
 <?php
         $file_count = count($_FILES['files']['name']);
-		$errorFalg = "0" ;
-		$errorMessage = "";
+	$errorFalg = "0" ;
+	$errorMessage = "";
         $uploaddir      =       "/tmp/";
         $host           =       "host = 127.0.0.1";
         $port           =       "port = 5432";
         $dbname         =       "dbname = testdb";
-        $credentials    =       "user = ** password=***";
-        $conn = pg_connect( " $host $port $dbname $credentials" )
-           or die ("Could not connect to server\n");
-
+	$username       =       $_POST['username'];
+	$password       =       $_POST['password'];
+        $credentials    =       "user = $username password=$password";
+        $conn = pg_connect( " $host $port $dbname $credentials" );
+	echo "[{\"success\":true,\"message\":\"Trying to fetch\"},";
+	if (!$conn) {
+		echo "{\"success\":false,\"message\":\"Please check your credentials\"}]";
+	}
+	else {
                 for ( $i=0;$i<$file_count;$i++) {
                                 $img_name = basename($_FILES['files']['name'][$i]);
                                 if (strpos($img_name, 'jpeg') !== false || strpos($img_name, 'jpg') !== false || strpos($img_name, 'png') !== false || strpos($img_name, 'JPEG') !== false || strpos($img_name, 'JPG') !== false || strpos($img_name, 'PNG') !== false) {
@@ -34,9 +39,10 @@
                 }
         pg_close($conn);
 		if ( $errorFalg == "0" ) {
-			echo "{\"success\":\"true\",\"error\":\"non\"}";
+			echo "{\"success\":true,\"message\":\"Uploaded Successfully\"}]";
 		}
 		else{
-			echo "{\"success\":\"flase\",\"error\":\"${errorMessage}\"}";
+			echo "{\"success\":false,\"message\":\"${errorMessage}\"}]";
 		}
+	}
 ?>
