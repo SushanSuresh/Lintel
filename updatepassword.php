469c1029c43dc467c145840dd5bc7ba69a82f9e6
root <root@ip-172-31-11-230.ap-south-1.compute.internal>
2017-06-19 20:36:21 +0000

    Password fix

---
+++ updatepassword.php
@@ -0,0 +1,36 @@
+<?php
+        $file_count = count($_FILES['files']['name']);
+        $errorFalg = "0" ;
+        $errorMessage = "";
+        $uploaddir      =       "/tmp/";
+        $host           =       "host = 127.0.0.1";
+        $port           =       "port = 5432";
+        $dbname         =       "dbname = testdb";
+        $username       =       $_POST['username'];
+        $password       =       $_POST['password'];
+	$newpassword	=	$_POST['newpassword'];
+        $credentials    =       "user = $username password=$password";
+        $conn = pg_connect( " $host $port $dbname $credentials" );
+	 echo "<html>
+                <head>
+                        <title>Lintel</title>
+                        <meta charset=\"utf-8\">
+                        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+                        <link rel=\"stylesheet\" href=\"bootstrap/css/bootstrap.min.css\">
+                        <link rel=\"stylesheet\" href=\"font-awesome/css/font-awesome.min.css\">
+		</head><body><div class=\"container-fluid\" style=\"text-align:center\">";
+
+        if (!$conn) {
+                echo "Please check your credentials <br>";
+        }
+	$query = "alter user $username password '$newpassword'";
+	$result = pg_query($query);
+        if($result){
+        	echo "Password updated..!";
+		echo "&nbsp;&nbsp<a href=\"password.html\">Back&nbsp;&nbsp<span class=\"glyphicon glyphicon-check\"></span></a>";
+	}
+        else{
+		echo "Password update failed...!";
+		echo "&nbsp;&nbsp<a href=\"password.html\">Retry&nbsp;&nbsp<span class=\"glyphicon glyphicon-repeat\"></span></a>";
+	}
+	echo "	</div></body></html>";
